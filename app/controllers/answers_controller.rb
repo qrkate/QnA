8acdf0bb92fa0a295f008c855b6d4000e75445e2
qr.kare <katekrasnyuk@gmail.com>
2021-05-28 17:19:22 +0300

    Answers controller & specs

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,25 @@
+class AnswersController < ApplicationController
+  def new
+    @answer = question.answers.new
+  end
+
+  def create
+    @answer = question.answers.create(answer_params)
+    if @answer.save
+      render :show
+    else
+      render :new
+    end
+  end
+
+  private
+  def question
+    question = Question.find(params[:question_id])
+  end
+
+  helper_method :question
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+end
