133b9f99ce5a43467fc3ea7777b67f8e719dcefa
qr.kare <katekrasnyuk@gmail.com>
2021-06-01 15:19:41 +0300

    Question index spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,6 +4,33 @@ RSpec.describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
   let(:user) { create(:user) }
 
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 3) }
+
+    before { get :index }
+
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
   describe 'GET #new' do
     before { login(user) }
 
