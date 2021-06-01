fe23695048f58daf16aaec68699391fda26487fe
qr.kare <katekrasnyuk@gmail.com>
2021-06-01 18:14:50 +0300

    Answer spec with authenticated&unauthenticated user

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
   let(:answer) { create(:answer) }
+  let(:user) { create(:user) }
+
+  before { login(user) }
 
   describe 'POST #create' do
     context 'with valid attributes' do
