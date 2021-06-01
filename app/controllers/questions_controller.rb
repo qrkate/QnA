7f8eea89c6d7f4cb918f8f47f957dd9182ac86e9
qr.kare <katekrasnyuk@gmail.com>
2021-06-02 00:40:14 +0300

    Question delete spec

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,13 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_question, only: [:show, :destroy]
 
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
-    @answer = Answer.new(question: @question)
+    @answer = Answer.new
   end
 
   def new
@@ -15,7 +15,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(question_params)
+    @question = current_user.questions.new(question_params)
 
     if @question.save
       redirect_to @question, notice: 'Your question successfully created.'
@@ -24,7 +24,20 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def destroy
+    if current_user.is_author?(@question)
+      @question.delete
+      redirect_to questions_path, notice: 'Your question successfully deleted.'
+    else
+      redirect_to questions_path, alert: 'You do not have permission to delete the question!'
+    end
+  end
+
   private
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
   def question_params
     params.require(:question).permit(:title, :body)
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many(:questions).dependent(:destroy) }
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe '#is_author?' do
+    let(:user) { create(:user) }
+
+    context 'when return true' do
+      it 'user is author of resource' do
+        question = create(:question, user: user)
+        expect(user.is_author?(question)).to be(true)
+      end
+    end
+
+    context 'when return false' do
+      it 'user is not author of resource' do
+        question = create(:question)
+        expect(user.is_author?(question)).to be(false)
+      end
+    end
+  end
 end
