9c61266014e282e0c4d8a836eb4d1b8017cdc3ce
qr.kare <katekrasnyuk@gmail.com>
2021-06-01 18:14:50 +0300

    Answer spec with authenticated&unauthenticated user

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -2,20 +2,34 @@ require 'rails_helper'
 
 feature 'User can create answer' do
   given(:question) { create(:question) }
+  given(:user) { create(:user) }
 
-  background { visit question_path(question) }
+  describe 'Authenticated user' do
 
-  scenario 'give an answer' do
-    fill_in 'Body', with: 'text text text'
-    click_on 'Answer'
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
 
-    expect(page).to have_content 'Your answer successfully created.'
-    expect(page).to have_content 'text text text'
+    scenario 'give an answer' do
+      fill_in 'Body', with: 'text text text'
+      click_on 'Answer'
+
+      expect(page).to have_content 'Your answer successfully created.'
+      expect(page).to have_content 'text text text'
+    end
+
+    scenario 'give an answer with errors' do
+      click_on 'Answer'
+
+      expect(page).to have_content "Body can't be blank"
+    end
   end
 
-  scenario 'give an answer with errors' do
+  scenario 'Unauthenticated user tries to give an answer' do
+    visit question_path(question)
     click_on 'Answer'
 
-    expect(page).to have_content "Body can't be blank"
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
