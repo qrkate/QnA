0aa4ab558817970669598940692d676ebc73d716
qr.kare <katekrasnyuk@gmail.com>
2021-06-01 17:35:06 +0300

    Answer create spec

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,18 +4,6 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
   let(:answer) { create(:answer) }
 
-  describe 'GET #new' do
-    before { get :new, params: { question_id: question.id } }
-
-    it 'assigns a new Answer to @answer' do
-      expect(assigns(:answer)).to be_a_new(Answer)
-    end
-
-    it 'renders new view' do
-      expect(response).to render_template :new
-    end
-  end
-
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves a new question in the database' do
@@ -24,7 +12,7 @@ RSpec.describe AnswersController, type: :controller do
 
       it 'redirects to show view' do
         post :create, params: { answer: attributes_for(:answer), question_id: question.id }
-        expect(response).to render_template :show
+        expect(response).to redirect_to assigns(:question)
       end
     end
 
@@ -33,9 +21,9 @@ RSpec.describe AnswersController, type: :controller do
         expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question.id } }.to_not change(Answer, :count)
       end
 
-      it "redirects to new view" do
+      it "redirects to question" do
         post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question.id }
-        expect(response).to render_template :new
+        expect(response).to render_template 'questions/show'
       end
     end
   end
